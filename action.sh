6d9569313490e3eff8172ef04f2aef4367d39618
buxiaomo <95112082@qq.com>
2021-01-17 20:48:47 +0800

    update

--- action.sh
+++ action.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
-function cleanup(){
-	if [ -f /swapfile ];then
+function cleanup() {
+	if [ -f /swapfile ]; then
 		sudo swapoff /swapfile
 		sudo rm -rf /swapfile
 	fi
@@ -23,7 +23,7 @@ function cleanup(){
 	sudo apt autoremove --purge -y
 }
 
-function init(){
+function init() {
 	[ -f sources.list ] && (
 		sudo cp -rf sources.list /etc/apt/sources.list
 		sudo rm -rf /etc/apt/sources.list.d/* /var/lib/apt/lists/*
@@ -39,14 +39,14 @@ function init(){
 	git config --global user.email "action@github.com"
 }
 
-function build(){
-	if [ -d openwrt ];then
+function build() {
+	if [ -d openwrt ]; then
 		pushd openwrt
 		git pull
 		popd
 	else
 		git clone https://github.com/openwrt/openwrt.git ./openwrt
-		[ -f ./feeds.conf.default ] && cat ./feeds.conf.default >> ./openwrt/feeds.conf.default
+		[ -f ./feeds.conf.default ] && cat ./feeds.conf.default >>./openwrt/feeds.conf.default
 	fi
 	pushd openwrt
 	./scripts/feeds update -a
@@ -60,14 +60,14 @@ function build(){
 	popd
 }
 
-function artifact(){
+function artifact() {
 	mkdir -p ./openwrt-gl-mifi
 	cp ./openwrt/bin/targets/ath79/generic/openwrt-ath79-generic-glinet_gl-mifi-squashfs-sysupgrade.bin ./openwrt-gl-mifi
-  cp ./openwrt/bin/targets/ath79/generic/config.buildinfo ./openwrt-gl-mifi
+	cp ./openwrt/bin/targets/ath79/generic/config.buildinfo ./openwrt-gl-mifi
 	zip -r openwrt-gl-mifi.zip ./openwrt-gl-mifi
 }
 
-function auto(){
+function auto() {
 	cleanup
 	init
 	build
